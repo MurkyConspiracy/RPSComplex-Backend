8fda7fe9ab605bd14b9de8a94bb025b9f445193a
MurkyConspiracy <nickreeder43@gmail.com>
2022-07-29 18:07:47 -0500

    Added netcode listening

--- src/netcode.py
+++ src/netcode.py
@@ -1,5 +1,6 @@
 import socket
 from loclogger import lprint
+import threading
 
 class netsock:
 
@@ -7,15 +8,31 @@ class netsock:
         lprint('Start netsock constructor')
         self._port = port
         self._hostname = socket.gethostname()
-        self._buildSocket()
+        self._socket = self._buildSocket()
+        netthread = threading.Thread(target=self._listen)
+        netthread.start()
     
 
-    def _buildSocket(self,) -> socket:
+    def _buildSocket(self,):
         lprint('Build socket')
         sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
         lprint('Binding..')
         sock.bind((self._hostname,self._port))
-        sock.listen(11)
         lprint('Socket established')
         return sock
+
+    def _listen(self):
+        lprint('Starting to listen...')
+        self._socket.listen(11)
+        lprint('Waiting for data...')
+        conn, addr = self._socket.accept()
+        with conn:
+            while True:
+                data = conn.recv(1024)
+                if not data:
+                    break
+                lprint(data)
+                
+
+    
 
