f4b26cae62266de3cee0a1d4c276cb1796c576d3
MurkyConspiracy <nickreeder43@gmail.com>
2022-07-29 17:01:07 -0500

    Basic netcode starting to be built

--- src/netcode.py
+++ src/netcode.py
@@ -1,2 +1,16 @@
 from audioop import add
 import socket
+
+class netsock:
+
+    _hostname = ''
+
+    def __init__(self,port):
+        self._port = port
+        self._hostname = socket.gethostbyname()
+    
+
+    def buildSocket(self,address=_hostname) -> socket:
+        sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        sock.bind(address, self._port)
+
